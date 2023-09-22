d5b38bd6815763bec99091b49bac7ec353f28dc1
himanoa <matsunoappy@gmail.com>
2023-09-22 22:05:55 +0900

    Setup yargs

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("hello")
+import yargs from "yargs"
+
+const args = yargs.option('dry-run', {
+  alias: 'd',
+  type: 'boolean',
+  description: 'Run the script without making any changes',
+}).parseSync()
+
+console.dir(args)
