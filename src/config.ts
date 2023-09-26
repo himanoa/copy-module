88dc6cde03418f45213065727b0cf9fb6bc6783b
himanoa <matsunoappy@gmail.com>
2023-09-27 03:53:52 +0900

    Implemented copymod config structure

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+import { CopyRule } from "./copy-rule";
+
+export type Config = {
+  rules: ReadonlyArray<CopyRule>,
+  ignorePatterns: ReadonlyArray<string>,
+}
---
+++ src/copy-rule.ts
@@ -0,0 +1,6 @@
+export type CopyRule = {
+  from: string;
+  to: string
+}
+
+
