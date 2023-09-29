1c0874a473a76b7d78e4a8cdf5bd2498d4529442
himanoa <matsunoappy@gmail.com>
2023-09-29 18:02:44 +0900

    Release 1.0.12

--- src/replace-file-path.ts
+++ src/replace-file-path.ts
@@ -1,6 +1,6 @@
 import ts from "typescript"
 import { readFileSync } from "fs"
-import source from "path"
+import path from "path"
 import fs from 'fs/promises'
 import { mkdirSync } from "fs"
 import { Config } from "./config"
@@ -10,8 +10,8 @@ import { makeIter } from "./dependency-tree"
 import { loadConfig } from 'tsconfig-paths'
 
 export const replaceFilePathCommand = async (filePath: string | null, dryRun: boolean) => {
-  const config = JSON.parse(readFileSync(source.join(process.cwd(), "dbc.config.json"), "utf-8")) as Config
-  const tsconfigPath = source.join(process.cwd(), 'tsconfig.json')
+  const config = JSON.parse(readFileSync(path.join(process.cwd(), "dbc.config.json"), "utf-8")) as Config
+  const tsconfigPath = path.join(process.cwd(), 'tsconfig.json')
   const tsconfig = ts.readConfigFile(tsconfigPath, (path) => readFileSync(path, 'utf8'))
 
   const configResult = loadConfig(tsconfigPath)
@@ -49,7 +49,7 @@ export const replaceFilePathCommand = async (filePath: string | null, dryRun: bo
         [key]: value
       }
     }, {})
-    await fs.writeFile(source.join(process.cwd(), "dbc.map.json"), JSON.stringify(serializableSourceToDestMap, null, 2))
+    await fs.writeFile(path.join(process.cwd(), "dbc.map.json"), JSON.stringify(serializableSourceToDestMap, null, 2))
   }
 }
 
@@ -61,7 +61,7 @@ export const copyModuleFromRules = async (rules: ReadonlyArray<CopyRule>, source
 
     const destinationPath = replaceFilePath(rule.from, rule.to, source)
     if(!dryRun) {
-      mkdirSync(source.dirname(destinationPath), { recursive: true }
+      mkdirSync(path.dirname(destinationPath), { recursive: true })
       await fs.copyFile(source, destinationPath)
     }
     if(dryRun) {
